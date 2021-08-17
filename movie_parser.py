f468c3ff951e8fb61234b01ff835188ebdcce557
Jose M Urena <urena.jose@gmail.com>
2021-08-17 00:12:25 -0400

    added code to read the movies_update file

--- movie_parser.py
+++ movie_parser.py
@@ -14,13 +14,18 @@ for row in data:
   genres_count = len(row[2].split("|"))
   mywriter.writerow([row[0],row[1],row[2],genres_count])
 
+output_file = open("movies_update.csv", newline='')
+output_reader = csv.reader(output_file)
+output_header = next(output_reader)
+output_data = [row for row in output_reader]
+
 if __name__ == "__main__":
     print(f"Original Header: {header}")
     print(f"Number Of Records: {num_rows}")
-    print("First 10 Movies listed")
+    print("\n First 10 Movies listed")
     for i in range(0,10):
         print(data[i])
-    print("Last 10 Movies listed")
+    print("\n Last 10 Movies listed")
     for i in range(len(data)-10,len(data)):
         print(data[i])
         
