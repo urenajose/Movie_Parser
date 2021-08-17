eb204105cff3fd0dd6029038df6eddeb5f7b8f91
Jose M Urena <urena.jose@gmail.com>
2021-08-17 00:50:28 -0400

    rename output file name to 'movies_updated', change print outputs to reflect movies_updated results

--- movie_parser.py
+++ movie_parser.py
@@ -1,10 +1,10 @@
 import csv
 
-input_file = open("movies.csv", newline='')
+input_file = open("movies.csv","r", newline='')
 myreader = csv.reader(input_file)
 header = next(myreader)
 data = [row for row in myreader]
-num_rows = len(data)
+input_file.close()
 output_file = open("movies_update.csv","w")
 mywriter = csv.writer(output_file)
 #creates header for my file
@@ -14,18 +14,20 @@ for row in data:
   genres_count = len(row[2].split("|"))
   mywriter.writerow([row[0],row[1],row[2],genres_count])
 
-output_file = open("movies_update.csv", newline='')
+output_file.close()
+
+output_file = open("movies_updated.csv","r", newline='')
 output_reader = csv.reader(output_file)
 output_header = next(output_reader)
 output_data = [row for row in output_reader]
+num_rows = len(output_data)
 
 if __name__ == "__main__":
-    print(f"Original Header: {header}")
+    print(f"Data fields: {output_header}")
     print(f"Number Of Records: {num_rows}")
     print("\n First 10 Movies listed")
     for i in range(0,10):
-        print(data[i])
+        print(output_data[i])
     print("\n Last 10 Movies listed")
-    for i in range(len(data)-10,len(data)):
-        print(data[i])
-        
+    for i in range(len(output_data)-10,len(output_data)):
+        print(output_data[i])
